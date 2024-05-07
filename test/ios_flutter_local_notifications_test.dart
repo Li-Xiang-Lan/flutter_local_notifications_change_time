3027f8755294d9a572de94790b4cbad94cf50563
chengmengyi <243237049@qq.com>
2024-05-07 17:35:13 +0800

    3

--- test/ios_flutter_local_notifications_test.dart
+++ test/ios_flutter_local_notifications_test.dart
@@ -1,7 +1,7 @@
 import 'package:clock/clock.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_local_notifications_change_time/flutter_local_notifications.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
